1a9278f3c362466c51ed4b72d3064b0f6154180f
Myles Horton <myles@getlantern.org>
2016-06-01 15:53:09 -0700

    added title

--- WindowsNotifier.go
+++ WindowsNotifier.go
@@ -11,7 +11,7 @@ type windowsNotifier struct {
 
 // Notify sends a notification to the user.
 func (n *windowsNotifier) Notify(msg *Notification) error {
-	cmd := exec.Command(n.path, "/m", msg.Message)
+	cmd := exec.Command(n.path, "/m", msg.Message, "/p", msg.Title)
 	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("Could not run command %v", err)
